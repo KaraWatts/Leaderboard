d97096ab2f9992a493c7a3cd2c9a4f4d5e01688b
Kara Watts <kdwwatts@gmail.com>
2024-07-14 11:25:38 -0400

    removed redundant FAQ

--- src/App.js
+++ src/App.js
@@ -159,10 +159,9 @@ const Data = [
       {/* <Quotes /> */}
     </div>
     <div className='p-3'>
-				<h1 style={{ textAlign: 'center', color: "limegreen"}}>FAQs</h1>
-        <h4 >10% Milestone Prizes</h4>
+				<h1 style={{ textAlign: 'center', color: "limegreen"}}>10% Milestone Prizes</h1>
 				<p style={{ marginTop:"1rem"}}>$10 from every challenger NOT in your household + 1 favor from every challenger IN your household</p>
-				<h4 >GRAND PRIZES</h4>
+				<h1 style={{ textAlign: 'center', color: "limegreen"}}>GRAND PRIZES</h1>
         <Table striped bordered hover>
           <tbody>
             <tr>
